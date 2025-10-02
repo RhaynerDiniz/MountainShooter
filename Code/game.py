274c6b27b91057afd7aeecae48bc96b358dddf65
Rhayner Diniz <dinizrhayner@gmail.com>
2025-10-02 17:48:26 -0300

    Background do menu criação da classe const

--- Code/game.py
+++ Code/game.py
@@ -1,5 +1,7 @@
 # game.py
 import pygame
+
+from Code.Const import WIN_WIDTH, WIN_HEIGTH
 from menu import Menu
 from level import Level
 
@@ -10,9 +12,9 @@ class Game:
     """
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGTH))
 
-    def run(self, ):
+    def run(self):
         #Inicia e gerencia o loop principal do jogo.
 
         while True:
