676b23a0020d3a532cd61097c88bf6d1d7b6e17f
Rhayner Diniz <dinizrhayner@gmail.com>
2025-10-02 15:33:14 -0300

    Criando loop principal conectando as classes game e menu

--- Code/game.py
+++ Code/game.py
@@ -10,21 +10,20 @@ class Game:
     """
     def __init__(self):
         pygame.init()
-        self.window: pygame.Surface = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Mountain Shooter")
+        self.window = pygame.display.set_mode(size=(600, 480))
 
-    def run(self):
-        """
-        Inicia e gerencia o loop principal do jogo.
-        """
+    def run(self, ):
+        #Inicia e gerencia o loop principal do jogo.
+
         while True:
             menu = Menu(self.window)
-            option = menu.run()
+            menu.run()
+            pass
 
-            if option == 1: # Se a opção 1 for escolhida no menu
-                level = Level(self.window, "Fase 1")
-                level.run() # Executa o nível
-            elif option == -1: # Se o usuário fechar a janela no menu
-                break
+            # check for all events
+            # for event in pygame.event.get():
+            #  if event.type == pygame.QUIT:
+            #        pygame.quit()  # fechar a janela
+            #        quit()  # fechar pygame
 
-        pygame.quit()
+
--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import pygame
+from Code.game import Game
 
-print('Setup START')
-pygame.init()
-window = pygame.display.set_mode(size = (600,480))
-print('Setup END')
-while True:
-    # check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # fechar a janela
-            quit() # fechar pygame
-
+game = Game()
+game.run()
