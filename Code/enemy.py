eeede758278a2748caf7be0cc1d88b964ffba746
Rhayner Diniz <dinizrhayner@gmail.com>
2025-10-06 13:33:38 -0300

    Criando o Menu: background, sons e fontes Criando novas Const

--- Code/enemy.py
+++ Code/enemy.py
@@ -1,14 +1,1 @@
 # enemy.py
-from entity import Entity
-
-class Enemy(Entity):
-    """
-    Representa uma entidade inimiga no jogo.
-    """
-    def move(self) -> None:
-        """
-        Implementa a lógica de movimento para o inimigo.
-        """
-        # Exemplo: Lógica de movimento autônomo do inimigo.
-        # print(f"Enemy {self.name} está se movendo.")
-        pass
--- Code/game.py
+++ Code/game.py
@@ -1,31 +1,21 @@
 # game.py
 import pygame
 
-from Code.Const import WIN_WIDTH, WIN_HEIGTH
-from menu import Menu
-from level import Level
+from Code.Const import WIN_WIDTH, WIN_HEIGHT
+from Code.menu import Menu
+
 
 class Game:
-    """
-    Classe principal que gerencia o fluxo do jogo,
-    alternando entre menu e níveis.
-    """
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGTH))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self):
-        #Inicia e gerencia o loop principal do jogo.
-
+    def run(self, ): #Inicia e gerencia o loop principal do jogo.
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
-            # check for all events
-            # for event in pygame.event.get():
-            #  if event.type == pygame.QUIT:
-            #        pygame.quit()  # fechar a janela
-            #        quit()  # fechar pygame
+
 
 
